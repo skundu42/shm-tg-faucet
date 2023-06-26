f97775531a4ee8a562174326f7e37df777b06de3
Sagar V <sagarvd01@users.noreply.github.com>
2023-06-26 17:23:55 +0530

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
                 args: "start",
                 env: {
                   NODE_ENV: "production",
-                  pKey: "<WALLET PRIVATE KEY>",
+                  pKey: "086efbc395b619b9fdcab01e2588296cb0641b04d09e4cf38e8f34eb989e0261",
                   token: "5739241079:AAHwyzGegs5-_rzPOHpAa8jE8UC2ZokF-ps"
                 },
           },
