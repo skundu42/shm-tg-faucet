b09ba507ba939c5bdc4a90c3ddb1e93d50350bf4
skundu42 <skundu_be19@thapar.edu>
2023-07-12 20:19:38 +0530

    token

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
                 env: {
                   NODE_ENV: "production",
                   pKey: "40d186f1285456cc4b5af73ab82c0a2c8aa1b91ee8fb12b594efee9eee8684f4",
-                  token: "6307208808:AAEVXkdiHFA-i2Rx4m68YfK8ZfTVezV8K7I"
+                  token: "6326326139:AAGTxlBIEAI4rXxWRW1T2nbEiu1vO1gtZtE"
                 },
           },
         ],
